82547e67e7773f53f062027a22c553b71ea3ede3
Victoria Popic <vpopic@broadinstitute.org>
2024-11-01 07:34:17 -0400

    removed unused modules and old utils

--- utils/panel_download.sh
+++
@@ -1,6 +0,0 @@
-INPUT_PANEL=$1
-
-while IFS="" read -r file || [ -n "$file" ]
-do
-  gsutil copy "${file}" .
-done < "${INPUT_PANEL}"
--- utils/split_vcf.sh
+++
@@ -1,5 +0,0 @@
-
-INPUT_VCF=$1
-for sample in $(bcftools query -l "${INPUT_VCF}"); do
-   bcftools view -c1 -Oz -s "$sample" -o "$sample".vcf.gz "${INPUT_VCF}"
-done
