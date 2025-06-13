92a53543d611ec6a174c644e280fd58d0d84be09
Victoria Popic <vpopic@broadinstitute.org>
2025-06-13 10:42:42 -0400

    Cli description update

--- engine/generate_dataset.py
+++ engine/generate_dataset.py
@@ -7,13 +7,11 @@ import engine.config as config_utils
 
 if __name__ == '__main__':
     # ------ CLI ------
-    parser = argparse.ArgumentParser(description='Filter and order set of graphs to generate training and validation datasets.')
+    parser = argparse.ArgumentParser(description='Generate graph datasets for training and validation.')
     parser.add_argument('--config', help='Dataset configuration YAML')
     args = parser.parse_args()
     # -----------------
 
-    # Load the config
     config = config_utils.load_config(args.config, config_type=config_utils.CONFIG_TYPE.DATA_GENERATION)
-
     random.seed(config.seed)
     graphs.graph_dataset.GraphDataset(config)
